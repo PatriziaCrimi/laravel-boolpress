25f9f858a24211ef17307a553a560e9e08656882
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-01-25 18:15:14 +0100

    Faker function added to create new Instances (dateTime bug to be fixed)

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -13,6 +13,17 @@ class PostsTableSeeder extends Seeder
    */
   public function run(Faker $faker)
   {
-    //
+    for($i=0; $i < 100; $i++) {
+      // Creating a new Object/Instance
+      $new_post_object = new Post();
+      $new_post_object->title = $faker->words(5, true);
+      $new_post_object->subtitle = $faker->sentence();
+      $new_post_object->author = $faker->name();
+      $new_post_object->cathegory = $faker->word();
+      $new_post_object->content = $faker->text();
+      $new_post_object->summary = $faker->paragraph();
+      $new_post_object->publication_date = $faker->date() $faker->time();
+      $new_post_object->save();
+    }
   }
 }
