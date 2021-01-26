56a3aaa720efd50bb055e92ed4182af32a2ef4bb
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-01-26 16:13:33 +0100

    Reset migrations, new migrations & populating db "posts" table with seeders & fakers

--- database/migrations/2021_01_25_155731_create_posts_table.php
+++ database/migrations/2021_01_25_155731_create_posts_table.php
@@ -15,11 +15,11 @@ class CreatePostsTable extends Migration
   {
     Schema::create('posts', function (Blueprint $table) {
       $table->id();
-      $table->string('title', 150);
-      $table->string('subtitle', 100)->nullable();
+      $table->string('title', 200);
+      $table->string('subtitle', 150)->nullable();
       $table->string('slug')->unique();
-      $table->string('cathegories', 30)->nullable();
-      $table->string('topics', 50)->nullable();
+      $table->string('cathegories', 100)->nullable();
+      $table->string('topics', 100)->nullable();
       $table->longText('content');
       $table->mediumText('summary')->nullable();
       $table->dateTime('publication_date')->nullable();
--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Post;
+use Illuminate\Support\Str;
 
 class PostsTableSeeder extends Seeder
 {
@@ -13,17 +14,40 @@ class PostsTableSeeder extends Seeder
    */
   public function run(Faker $faker)
   {
-    for($i=0; $i < 100; $i++) {
+    // Creating 100 fake posts (100 lines in the table "Posts")
+    for($i = 0; $i < 100; $i++) {
       // Creating a new Object/Instance
-      $new_post_object = new Post();
-      $new_post_object->title = $faker->words(3, true);
-      $new_post_object->subtitle = $faker->sentence(4);
-      $new_post_object->author = $faker->name();
-      $new_post_object->cathegory = $faker->word();
-      $new_post_object->content = $faker->text();
-      $new_post_object->summary = $faker->paragraph();
-      $new_post_object->publication_date = $faker->datetime();
-      $new_post_object->save();
+      $new_post = new Post();
+      $new_post->title = $faker->sentence(5);
+      $new_post->subtitle = $faker->sentence(4);
+      $new_post->cathegories = $faker->words(3, true);
+      $new_post->topics = $faker->words(5, true);
+      $new_post->content = $faker->text();
+      $new_post->summary = $faker->paragraph();
+      $new_post->publication_date = $faker->datetime();
+      $new_post->notes = $faker->sentence();
+      // Generating the slug
+      $slug = Str::slug($new_post->title);
+      $root_slug = $slug;
+      /*
+        Checking that the slug is UNIQUE -> it doesn't exist already in my db
+        QUERY: SELECT 'slug' FROM 'posts' WHERE slag = $slag
+        We need ->first() because "where" restituisce una COLLECTION and I need only the first result
+      */
+      $current_post = Post::where('slug', $slug)->first();
+      $counter = 1;
+      // The while loop starts when a post with the same slug is found
+      while($current_post) {
+        // Generating a new slug with a final number to make it different
+        $slug = $root_slug . '-' . $counter;
+        $counter++;
+        // New QUERY to check if there is another post with this new slug
+        $current_post = Post::where('slug', $slug)->first();
+      }
+      // Assigning the final unique slug
+      $new_post->slug = $slug;
+      // Saving all the data just generated
+      $new_post->save();
     }
   }
 }
