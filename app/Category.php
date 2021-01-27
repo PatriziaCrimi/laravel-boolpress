58cb961b787020e584b07416fde851c4e09f7de2
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-01-27 17:37:39 +0100

    Mapping tables relationship in Models "Post" & "Category"

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+  // This function connects the two Models: it creates the relationship to the posts (PLURAL: one category can be given to many different posts)
+  // ONE TO MANY - PRIMARY TABLE needS the "hasMany()" function
+  public function posts() {
+    return $this->hasMany('App\Post');
+  }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-  //
+  // This function connects the two Models: it creates the relationship to the category (SINGULAR: only one category can be given to one post)
+  // ONE TO MANY - SECONDARY TABLE needs the "belongsTo()" function
+  public function category() {
+    return $this->belongsTo('App\Category');
+  }
 }
