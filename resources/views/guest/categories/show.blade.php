923ca7f177273c671077c4308cf2241a86f38202
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-01-29 16:21:29 +0100

    Printing on screen all posts with a specified category

--- resources/views/guest/categories/show.blade.php
+++ resources/views/guest/categories/show.blade.php
@@ -10,5 +10,19 @@
                 </h1>
             </div>
         </div>
+        <div class="row">
+            <div class="col-12">
+                <ul>
+                  {{-- Ciclo la COLLECTION di oggetti "$post" a cui accedo tramite la relazione "posts()" creata nel Model "Category" --}}
+                  @foreach ($category->posts as $post)
+                    <li>
+                      <a href="{{route('posts.show', ['slug'=> $post->slug])}}">
+                        {{$post->title}}
+                      </a>
+                    </li>
+                  @endforeach
+                </ul>
+            </div>
+        </div>
     </div>
 @endsection
